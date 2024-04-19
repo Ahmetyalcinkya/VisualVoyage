5ec149affb0fe028f2c429f8f6a21b84f99db535
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-19 13:56:28 +0300

    User entity added! Pom.xml updated!

---
+++ src/main/java/com/vv/VisualVoyage/entities/User.java
@@ -0,0 +1,43 @@
+package com.vv.VisualVoyage.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user", schema = "social")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "gender")
+    private String gender;
+
+    private List<Integer> followers;
+
+    private List<Integer> followings;
+
+}
