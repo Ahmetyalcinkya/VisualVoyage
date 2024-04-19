88d86f9b68d508bf67005c26e1ec10bdadc34385
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-20 01:42:15 +0300

    Liked relation w/user changed to ManyToMany

--- src/main/java/com/vv/VisualVoyage/entities/Post.java
+++ src/main/java/com/vv/VisualVoyage/entities/Post.java
@@ -35,7 +35,7 @@ public class Post {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @OneToMany //TODO CAN BE REPLACED WITH -> MANY TO MANY
+    @ManyToMany
     private Set<User> liked = new HashSet<>();
 
     @ManyToOne
