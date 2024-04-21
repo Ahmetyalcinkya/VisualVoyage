6d3748c6d0b75440c91b3b5acdaae155536fd8ba
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-21 13:29:50 +0300

    parseClaimsJwt replaced with parseClaimsJws

--- src/main/java/com/vv/VisualVoyage/utils/JwtProvider.java
+++ src/main/java/com/vv/VisualVoyage/utils/JwtProvider.java
@@ -24,7 +24,7 @@ public class JwtProvider {
 
     public static String getEmailFromJwt(String jwt){
         jwt = jwt.substring(7);
-        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJwt(jwt).getBody();
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
 
         return String.valueOf(claims.get("email"));
     }
